0340fdb3ef73a2bc959e0ebdd0b6a5818e4f3da1
sreenivasulu.paruchuri1 <sreenivasulu.paruchuri@gmail.com>
2025-07-18 15:29:05 +0530

    intial discourse

--- .devcontainer/scripts/start.rb
+++ .devcontainer/scripts/start.rb
@@ -3,10 +3,10 @@
 
 puts "👋 Welcome to the Discourse devcontainer! Let's get everything ready..."
 
-puts "Setting permissions on volume mounts..."
-system "sudo chown discourse .", exception: true
-system "sudo chown discourse node_modules", exception: true
-system "sudo chown -R postgres /shared/postgres_data", exception: true
+# puts "Setting permissions on volume mounts..."
+# system "sudo chown discourse .", exception: true
+# system "sudo chown discourse node_modules", exception: true
+# system "sudo chown -R postgres /shared/postgres_data", exception: true
 
 puts "Starting services..."
 fork do
